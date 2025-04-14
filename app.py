130a3299051878ef88dfedba9e83d5104943657b
cyberonax <vocybersyn@gmail.com>
2025-04-14 21:31:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -939,12 +939,12 @@ Aluminum, Coal, Gold, Iron, Lead, Lumber, Marble, Oil, Pigs, Rubber, Uranium, Wa
                     
                     total_peace_mismatch = unique_peaceA + unique_peaceB + unique_peaceC
                     
-                    st.write(f"**Peacetime Mismatches Breakdown by Level**")
+                    st.markdown("#### Peacetime Mismatches Breakdown by Level:")
                     st.write(f"- **Level A** (< 1000 days old): **{unique_peaceA}**")
                     st.write(f"- **Level B** (1000 to 2000 days old): **{unique_peaceB}**")
-                    st.write(f"-**Level C** (>= 2000 days old): **{unique_peaceC}**")
-                    st.write(f"**Total Peacetime Mismatch among Complete Trade Circles:** {total_peace_mismatch}")
-                    st.write(f"**Wartime Mismatch among Complete Trade Circles:** {unique_wartime_mismatch} ({wartime_mismatch_percentage:.2f}%)")
+                    st.write(f"- **Level C** (>= 2000 days old): **{unique_peaceC}**")
+                    st.markdown("#### Total Peacetime Mismatch among Complete Trade Circles:** {total_peace_mismatch}")
+                    st.markdown("#### Wartime Mismatch among Complete Trade Circles: {unique_wartime_mismatch} ({wartime_mismatch_percentage:.2f}%)")
                     st.markdown('---')
 
                     st.subheader("Action Plan for Alliance Management")
